392e221e64f1537bb2d6481ccc7d7cfa3d744fca
Ryan Stewart <ryanstewartalex@gmail.com>
2023-06-06 00:46:39 -0500

    Update Demo Website

--- pride-html.js
+++ pride-html.js
@@ -10,14 +10,16 @@ const colorIncrement = 3;
 
 class PrideHTML
 {
-    #gradients = {
-        "traditional": ["#e40203", "#ff8b00", "#feed00", "#008026", "#004dff", "#750686"],
-        "philadelphia": ["#000000", "#784f17", "#FF0000", "#FF8E00", "#FFFF00", "#008E00", "#00C0C0", "#400098", "#8E008E"],
-        "progress": ["#FFFFFF", "#ffafc8", "#74d7ee", "#784f17", "#000000", "#FF0000", "#FF8E00", "#FFFF00", "#008E00", "#00C0C0", "#400098", "#8E008E"],
-        "trans": ["#61cbfe", "#f6a8b7", "#fffefd"],
-        "non-binary": ["#ffdc00", "#ffffff", "#af5cd8", "#000000"],
-        "intersex": []
-    };
+    // #gradients = {
+    //     "traditional": ["#e40203", "#ff8b00", "#feed00", "#008026", "#004dff", "#750686"],
+    //     "philadelphia": ["#000000", "#784f17", "#FF0000", "#FF8E00", "#FFFF00", "#008E00", "#00C0C0", "#400098", "#8E008E"],
+    //     "progress": ["#FFFFFF", "#ffafc8", "#74d7ee", "#784f17", "#000000", "#FF0000", "#FF8E00", "#FFFF00", "#008E00", "#00C0C0", "#400098", "#8E008E"],
+    //     "trans": ["#61cbfe", "#f6a8b7", "#fffefd"],
+    //     "non-binary": ["#ffdc00", "#ffffff", "#af5cd8", "#000000"],
+    //     "intersex": []
+    // };
+
+    #flagGradient = ["#e40203", "#ff8b00", "#feed00", "#008026", "#004dff", "#750686"];
 
     constructor(angle = 135, speed = 1)
     {
@@ -119,18 +121,17 @@ class PrideHTML
         return `#${f(0)}${f(8)}${f(4)}`;
     }
 
-    colorTextElement(cssSelector, flag)
+    colorTextElement(cssSelector)
     {
         const elements = document.querySelectorAll(cssSelector);
-        const flagGradient = this.#gradients[flag];
-        if (!elements || !flagGradient) return;
+        if (!elements) return;
 
         elements.forEach(el => {
             el.classList.add("pride-html-gradient")
-            this.#colorText(el, flagGradient);
+            this.#colorText(el, this.#flagGradient);
         });
 
-        let gradient = flagGradient;
+        let gradient = this.#flagGradient;
         setInterval(() =>
         {
             const newGradient = [];
